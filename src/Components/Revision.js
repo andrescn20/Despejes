b864e906f60d3bcaa398210a8b0d227fc70cd8e0
Andres C <andrescn20@gmail.com>
2022-09-14 22:17:10 -0600

    Agrega metodo para revisar si el despeje es correcto

--- src/Components/Revision.js
+++ src/Components/Revision.js
@@ -1,4 +1,4 @@
-// import { useState } from 'react';
+import React from 'react';
 import Equation from './despejes';
 import nerdamer from 'nerdamer/all.min';
 import { BlockMath } from 'react-katex';
@@ -6,12 +6,29 @@ import { BlockMath } from 'react-katex';
 export default function Revision(props) {
   const { equation, variable } = props;
   const currentEquation = new Equation(equation);
+  const [solutionStatus, setSolutionStatus] = React.useState('');
 
   currentEquation.setEquation();
   const solution = nerdamer(`solve(${equation}, ${variable})`);
   const cleanSolution = solution.toString().replace(/\[|\]/g, '');
   const latexSolution = nerdamer.convertToLaTeX(`${cleanSolution}`);
-  console.log(cleanSolution);
 
-  return <BlockMath math={`${variable}=${latexSolution}`} />;
+  function revisar() {
+    if (
+      currentEquation.eqLeft === cleanSolution ||
+      currentEquation.eqRight === cleanSolution
+    ) {
+      setSolutionStatus('Variable despejada con exito');
+    } else {
+      setSolutionStatus('Variable pendiente de despejar');
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={revisar}>Revisar</button>
+      <p>{solutionStatus}</p>
+      <BlockMath math={`${variable}=${latexSolution}`} />
+    </div>
+  );
 }
