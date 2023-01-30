a549b6f4fbfa45c99bacd7674cebe0318fa765b1
andrescn20 <andrescn20@gmail.com>
2023-01-29 18:22:12 -0600

    Format Document

--- src/Components/Operacion.js
+++ src/Components/Operacion.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-export default function Operacion({text}) {
-
-     return  (  <div className='flex'>
-             <button type='submit' className= "rounded-xl px-4 py-1 shadow-md shadow-gray-400 mx-2 self-center grow my-1">
-                     {text}
-                   </button>
-         </div>)
-  
+export default function Operacion({ text }) {
+  return (
+    <div className='flex'>
+      <button
+        type='submit'
+        className='rounded-xl w-24 py-1 shadow-md shadow-gray-400 self-center  my-1'
+      >
+        {text}
+      </button>
+    </div>
+  );
 }
