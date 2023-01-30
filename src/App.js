7727021c91926ac6546561bcf5b9b2bb80bfbda9
andrescn20 <andrescn20@gmail.com>
2023-01-29 22:12:22 -0600

    Layout

--- src/App.js
+++ src/App.js
@@ -57,7 +57,10 @@ function App() {
     setCurrentFactor('');
   };
 
-  const clearEquation = () => {};
+  const clearEquation = () => {
+    addToHistory('');
+    setCurrentFactor('');
+  };
 
   const goBack = () => {
     if (history.length === 1) {
@@ -96,92 +99,94 @@ function App() {
   };
 
   return (
-    <div className=''>
+    <div className='flex flex-col'>
       <Header />
-      <div className='flex flex-col items-center gap-4'>
-        <h2>Operaciones</h2>
-        <p>Introduzca la ecuación para la cuál desea despejar</p>
-        <form className='flex flex-col px-8' onSubmit={submitEquation}>
-          <input
-            className='bg-slate-200 p-2 mb-4 mx-2 rounded-xl font-medium text-center w-96 shadow-lg self-center border-2-white'
-            type='text'
-            placeholder='x_f - x_i = v_i*t+(1/2)(a)(t^2)'
-            onChange={handleEquationChange}
-          ></input>
-          <Boton text='Añadir Ecuación' type='submit' />
-        </form>
-
-        <InputField equation={equation} />
-        <div className=''>
-          <Boton onClick={goBack} text='Deshacer' isRed={true} />
-          <Boton onClick={clearEquation} text='Limpiar' isRed={true} />
+      <div className='grow justify-center'>
+        <div className='grid grid-cols-4 items-center gap-4 my-12 px-12'>
+          <div className='flex flex-col items-center col-span-2 gap-4  row-span-3'>
+            <h2>Operaciones</h2>
+            <p>Introduzca la ecuación para la cuál desea despejar</p>
+            <form className='flex flex-col px-8' onSubmit={submitEquation}>
+              <input
+                className='bg-slate-200 p-2 mb-4 mx-2 rounded-xl font-medium text-center w-96 shadow-lg self-center border-2-white'
+                type='text'
+                placeholder='x_f - x_i = v_i*t+(1/2)(a)(t^2)'
+                onChange={handleEquationChange}
+              ></input>
+              <Boton text='Añadir Ecuación' type='submit' />
+            </form>
+            <InputField equation={equation} />
+            <div className=''>
+              <Boton onClick={goBack} text='Deshacer' isRed={true} />
+              <Boton onClick={clearEquation} text='Limpiar' isRed={true} />
+            </div>
+          </div>
+          <div className='grid grid-cols-3 justify-center gap-2 col-span-2 row-span-2 col-start-3'>
+            <Sumar
+              className='self-center'
+              name={'Sumar'}
+              handleOperation={handleOperation}
+              equation={equation}
+              factor={currentFactor}
+            />
+            <Restar
+              name={'Restar'}
+              handleOperation={handleOperation}
+              equation={equation}
+              factor={currentFactor}
+            />
+            <Multiplicar
+              name={'Multiplicar'}
+              handleOperation={handleOperation}
+              equation={equation}
+              factor={currentFactor}
+            />
+            <Dividir
+              name={'Dividir'}
+              handleOperation={handleOperation}
+              equation={equation}
+              factor={currentFactor}
+            />
+            <Potencia
+              name={'Potencia'}
+              handleOperation={handleOperation}
+              equation={equation}
+              factor={currentFactor}
+            />
+            <Raiz
+              name={'Raiz'}
+              handleOperation={handleOperation}
+              equation={equation}
+              factor={currentFactor}
+            />
+            <Simplificar
+              name={'Simplificar'}
+              handleOperation={handleOperation}
+              equation={equation}
+              factor={currentFactor}
+            />
+            <Expandir
+              name={'Expandir'}
+              handleOperation={handleOperation}
+              equation={equation}
+              factor={currentFactor}
+            />
+          </div>
+          <div className='col-span-2 col-start-1 row-span-2'>
+            <Factores
+              equation={equation}
+              changeCurrentFactor={changeCurrentFactor}
+            />
+          </div>
+          <div className='flex justify-center my-6 col-span-2 row-span-3 col-start-3 row-start-3'>
+            <RevisionContainer
+              submitVariable={submitVariable}
+              handleVariableChange={handleVariableChange}
+              equation={equation}
+              variable={variable}
+            />
+          </div>
         </div>
-
-        <Factores
-          equation={equation}
-          changeCurrentFactor={changeCurrentFactor}
-        />
-
-        <div className='grid grid-cols-3 justify-center gap-2'>
-          <Sumar
-            className='self-center'
-            name={'Sumar'}
-            handleOperation={handleOperation}
-            equation={equation}
-            factor={currentFactor}
-          />
-          <Restar
-            name={'Restar'}
-            handleOperation={handleOperation}
-            equation={equation}
-            factor={currentFactor}
-          />
-          <Multiplicar
-            name={'Multiplicar'}
-            handleOperation={handleOperation}
-            equation={equation}
-            factor={currentFactor}
-          />
-          <Dividir
-            name={'Dividir'}
-            handleOperation={handleOperation}
-            equation={equation}
-            factor={currentFactor}
-          />
-          <Potencia
-            name={'Potencia'}
-            handleOperation={handleOperation}
-            equation={equation}
-            factor={currentFactor}
-          />
-          <Raiz
-            name={'Raiz'}
-            handleOperation={handleOperation}
-            equation={equation}
-            factor={currentFactor}
-          />
-          <Simplificar
-            name={'Simplificar'}
-            handleOperation={handleOperation}
-            equation={equation}
-            factor={currentFactor}
-          />
-          <Expandir
-            name={'Expandir'}
-            handleOperation={handleOperation}
-            equation={equation}
-            factor={currentFactor}
-          />
-        </div>
-      </div>
-
-      <div className='flex justify-center my-6'>
-        <RevisionContainer
-          submitVariable={submitVariable}
-          handleVariableChange={handleVariableChange}
-          equation={equation}
-          variable={variable}
-        />
       </div>
       <Footer />
     </div>
