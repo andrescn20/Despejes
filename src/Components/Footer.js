710d0717189705fdfde99bac08093e5b88d3d2a4
andrescn20 <andrescn20@gmail.com>
2023-01-29 22:12:48 -0600

    Push Footer to Bottom

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='bg-slate-200 flex justify-around h-10 items-center'>
+    <footer className='bg-slate-200 flex justify-around h-10 items-center fixed bottom-0 w-full'>
       <p className='flex align-center'>Created By:</p>
       <a href='https://github.com/andrescn20' className='flex gap-2'>
         <p>
