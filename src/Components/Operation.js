7bda483ce77959432dc1b2aa6e741fb2ab0f9836
andrescn20 <andrescn20@gmail.com>
2023-04-02 23:46:36 -0600

    New component for all operations

---
+++ src/Components/Operation.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+export default function Operation({ equation, name, handleOperation, factor }) {
+  const [newEquation, setNewEquation] = useState('');
+
+  useEffect(() => {
+    setNewEquation(equation);
+  }, []);
+
+  useEffect(() => {
+    handleOperation(newEquation);
+  }, [newEquation]);
+
+  const updateEquation = (e) => {
+    e.preventDefault();
+
+    fetch(`/${name}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        equation: newEquation,
+        factor: factor,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setNewEquation(data.result);
+      });
+  };
+
+  return (
+    <form onSubmit={updateEquation}>
+      <div className='flex'>
+        <button
+          type='submit'
+          className='rounded-lg w-24 py-1 shadow-sm shadow-gray-500 self-center  my-1 active:activeFactor'
+        >
+          {name}
+        </button>
+      </div>
+    </form>
+  );
+}
