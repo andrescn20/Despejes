5222e97305edde5bb1b11373e4b6ec7968969a06
Andres Castro <andrescn20@gmail.com>
2023-04-07 00:36:04 -0600

    Update styling

--- src/Components/Boton.js
+++ src/Components/Boton.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Boton({text, isRed, onClick}) {
 
 
-        return <button onClick={onClick} className={`${isRed ? 'bg-red-500' : 'bg-green-400'} rounded-xl px-4 py-1 text-white shadow-md
-         shadow-gray-400 mx-2 w-fit self-center font-bold tracking-widest`}>
+        return <button onClick={onClick} className={`${isRed ? 'bg-red-500' : 'bg-green-400'} text-white 
+         mx-2 font-bold tracking-widest rounded-lg w-24 py-1 shadow-sm shadow-gray-500 self-center  my-1`}>
         {text}
       </button>      
   
