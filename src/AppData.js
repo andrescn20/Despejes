1e0a2692275a723056ab5a32753893baed2500a3
Andres Castro <andrescn20@gmail.com>
2023-04-10 12:29:22 -0600

    Update Data Format

--- src/AppData.js
+++ src/AppData.js
@@ -10,53 +10,59 @@ const AppData = {
       equations: [
           {
             sympy: "Eq(v, d/t)",
-            latex: "v=d/t",
+            latex: "v=\\frac{d}{t}",
             factors: ["v", "d", "t"],
             variables: ["d", "t"], //v no es una variable, ¡Porque ya está despejada!
           },
           {
             sympy: "Eq(a, (v_f-v_i)/t)",
-            latex: "a=(v_f-v_i)/t",
-            factors: ["a", "v_f", "v_i", "t", "a*t"],
+            latex: "a=\\frac{v_f-v_i}{t}",
+            factors: ["a", "v_f", "v_i", "t", "a \\cdot t"],
             variables: ["v_f", "v_i", "t"],
           },
           {
             sympy: "Eq(v_f**2, v_i**2 + 2 * a * d)",
             latex: "v_f^2=v_i^2 + 2 \\cdot a \\cdot d",
-            factors: ["a", "v_f", "v_i", "d", "2", "v_f**2", "v_i**2", "a * d"],
+            factors: ["a", "v_f", "v_i", "d", "2", "v_f^2", "v_i^2", "a \\cdot d"],
             variables: ["a", "v_f", "v_i", "d"],
           },
           {
             sympy: "Eq(d, v_i*t+a*t**2/2)",
-            latex: "d=v_i\\cdot t+a\\cdot t^2/2",
-            factors: ["d", "v_i", "a", "t", "2", "v_i*t", "at**2/2"],
+            latex: "d=v_i\\cdot t+a\\cdot \\frac{t^2}{2}",
+            factors: ["d", "v_i", "a", "t", "2", "v_i \\cdot t", "\\frac{at^2}{2}"],
             variables: ["v_i", "a", "t", "2"],
           },
           {
             sympy: "Eq(d, (v_f+v_i)*t/2)",
             latex: "d=(v_f+v_i)\\cdot t/2",
-            factors: ["d","v_i","v_f","t","2","(v_f + v_i)","(v_f + v_i) * t / 2",],
+            factors: ["d","v_i","v_f","t","2","(v_f + v_i)","(v_f + v_i) \\cdot t / 2",],
             variables: ["v_i", "v_f", "t"],
           },
         ]},
       {name: 'Electromagnetismo',
       equations:[
         {
-          sympy: "Eq(E, d/t)",
-          latex: "E=(k/r^2)(q_1\\cdot q_2)",
+          sympy: "Eq(E , (k/r**2)(q_1))",
+          latex: "E=(k/r^2)(q_1)",
           factors: ["k", "q_1", "q_2", "r"],
           variables: ["d", "t"], //v no es una variable, ¡Porque ya está despejada!
         },
         {
-          sympy: "Eq(v, d/t)",
-          latex: "v=d/t",
-          factors: ["v", "d", "t"],
+          sympy: "Eq(F , (k/r**2)(q_1*q_2))",
+                    latex: "F=(k/r^2)(q_1\\cdot q_2)",
+          factors: ["k", "q_1", "q_2", "r"],
           variables: ["d", "t"], //v no es una variable, ¡Porque ya está despejada!
         },
         {
-          sympy: "Eq(v, d/t)",
-          latex: "v=d/t",
-          factors: ["v", "d", "t"],
+          sympy: "Eq(V , (k/r)(q_1))",
+                    latex: "V=(k/r)(q_1)",
+          factors: ["k", "q_1", "q_2", "r"],
+          variables: ["d", "t"], //v no es una variable, ¡Porque ya está despejada!
+        },
+        {
+          sympy: "Eq(U , (k/r)(q_1*q_2))",
+                    latex: "U=(k/r)(q_1\\cdot q_2)",
+          factors: ["k", "q_1", "q_2", "r"],
           variables: ["d", "t"], //v no es una variable, ¡Porque ya está despejada!
         },
       ]}
