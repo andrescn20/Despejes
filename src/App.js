042182f61668895412759e98e8020348f9fba988
Andres Castro <andrescn20@gmail.com>
2023-04-10 23:05:32 -0600

    Upgrade Loading Modal

--- src/App.js
+++ src/App.js
@@ -44,10 +44,10 @@ function App() {
   function LoadingModal() {
    if(isLoading){
     return (
-      <div className='absolute h-full w-full bg-black/40 flex items-center justify-center'>
+      <div className='absolute h-full w-full bg-black/40 flex items-center justify-center z-50'>
         <div className='flex items-center justify-center gap-4 bg-header border-2 rounded-lg p-2'>
         <svg className="animate-spin h-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <g> <path fill="none" d="M0 0h24v24H0z"/> <path stroke="white" fill="white" d="M12 2a1 1 0 0 1 1 1v3a1 1 0 0 1-2 0V3a1 1 0 0 1 1-1zm0 15a1 1 0 0 1 1 1v3a1 1 0 0 1-2 0v-3a1 1 0 0 1 1-1zm8.66-10a1 1 0 0 1-.366 1.366l-2.598 1.5a1 1 0 1 1-1-1.732l2.598-1.5A1 1 0 0 1 20.66 7zM7.67 14.5a1 1 0 0 1-.366 1.366l-2.598 1.5a1 1 0 1 1-1-1.732l2.598-1.5a1 1 0 0 1 1.366.366zM20.66 17a1 1 0 0 1-1.366.366l-2.598-1.5a1 1 0 0 1 1-1.732l2.598 1.5A1 1 0 0 1 20.66 17zM7.67 9.5a1 1 0 0 1-1.366.366l-2.598-1.5a1 1 0 1 1 1-1.732l2.598 1.5A1 1 0 0 1 7.67 9.5z"/> </g> </svg>
-        <span className='text-white text-lg'>Calculating...</span></div>
+        <span className='text-white text-lg'>Calculando...</span></div>
       </div>
     )
    }
