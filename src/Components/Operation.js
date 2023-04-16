30eddcfe0816221fef4ef89ba0ee84803fec5a9f
Andres Castro <andrescn20@gmail.com>
2023-04-16 15:01:13 -0600

    Updates

--- src/Components/Operation.js
+++ src/Components/Operation.js
@@ -37,7 +37,9 @@ export default function Operation({ equation, name, handleOperation, factor }) {
           type='submit'
           className='rounded-lg w-24 py-1 shadow-sm shadow-gray-500 self-center  my-1 active:activeFactor'
         >
-          {name}
+         <span className="w-6 h-6 flex justify-center items-center">
+           {symbol}
+          </span>
         </button>
       </div>
     </form>
